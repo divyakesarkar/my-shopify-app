cace46dcbe3cb3d469788a77f1d6434662ecd7e8
divyakesarkar <156050692+divyakesarkar@users.noreply.github.com>
2024-01-09 16:33:01 +0530

    Update product.js

--- app/routes/api/product.js
+++ app/routes/api/product.js
@@ -1,3 +1,16 @@
-// In a file like routes/api/product.js
 
+import { PrismaClient } from '@prisma/client';
 
+export const loader = async ({ params }) => {
+    const prisma = new PrismaClient();
+  const productId = params.productId;
+  const product = await prisma.product.findUnique({
+    where: { id: productId },
+  });
+
+  return new Response(JSON.stringify(product), {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
